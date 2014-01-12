61f7d1980c447220fb41ff3b59c8a4310797002b
Tom Vaughan <tvaughan@tocino.cl>
2014-01-11 22:41:45 -0300

    Run slamhound and other clean-ups.

--- src/ring/adapter/grizzly.clj
+++ src/ring/adapter/grizzly.clj
@@ -1,11 +1,12 @@
 (ns ring.adapter.grizzly
-  (:require
-   [ring.util.servlet :refer [build-request-map update-servlet-response]])
-  (:import
-   (org.glassfish.grizzly.http.server HttpServer NetworkListener)
-   (org.glassfish.grizzly.servlet WebappContext)
-   (javax.servlet.http HttpServlet)
-   (java.lang Thread)))
+  (:require [ring.util.servlet :refer [build-request-map
+                                       update-servlet-response]])
+  (:import (javax.servlet.http HttpServlet)
+           (org.glassfish.grizzly.http.server HttpServer
+                                              NetworkListener)
+           (org.glassfish.grizzly.servlet WebappContext)))
+
+(def servlet-name "grizzly")
 
 (defn- create-servlet
   "Create a proxied HttpServlet implementation for a given Ring handler."
@@ -21,22 +22,22 @@
   "Create a Grizzly HttpServer instance."
   [options]
   (let [server (HttpServer.)]
-    (.addListener server (NetworkListener. "grizzly" (options :host "0.0.0.0") (options :port 80)))
+    (.addListener server (NetworkListener. servlet-name (options :host "0.0.0.0") (options :port 80)))
     server))
 
 (defn- deploy-server
   "Deploy a Grizzly HttpServer instance with a given HttpServlet instance."
   [server servlet]
-  (let [ctx (WebappContext. "grizzly" "/")]
-    (.addServlet ctx "grizzly" servlet)
-    (.deploy ctx server)))
+  (doto (WebappContext. servlet-name "/")
+    (.addServlet servlet-name servlet)
+    (.deploy server))
+  server)
 
 (defn run-grizzly
   "Start a Grizzly HTTP server to serve the given handler according to the supplied options:"
   [handler options]
   (let [server (create-server (dissoc options :configurator))]
-    (deploy-server server (create-servlet handler))
-    (.start server)
+    (.start (deploy-server server (create-servlet handler)))
     (when (:join? options true)
       (.join (Thread/currentThread)))
     server))
